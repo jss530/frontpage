48636b599a77056b16bd118afb5768580e6dfb8a
jss530 <jsifre01@gmail.com>
2018-05-29 21:44:38 -0400

    Changed navbar categories

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -19,7 +19,7 @@ class Navigation extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Navbar</a>
+        <a className="navbar-brand" href="/">Welcome!</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -29,21 +29,22 @@ class Navigation extends React.Component {
           <ul className="navbar-nav mr-auto">
 
             <NavItem path="/" name="Home" />
-            <NavItem path="/page2" name="Page2" />
-            <NavItem path="/page3" name="Disabled" disabled="true" />
+            <NavItem path="/page2" name="About" />
+            <NavItem path="/page3" name="Your Account" disabled="true" />
 
             <li className="nav-item dropdown">
-              <NavDropdown name="Dropdown">
-                <a className="dropdown-item" href="/">Action</a>
-                <a className="dropdown-item" href="/">Another action</a>
+              <NavDropdown name="Browse Categories">
+                <a className="dropdown-item" href="/">Red</a>
+                <a className="dropdown-item" href="/">White</a>
+                <a className="dropdown-item" href="/">Sparkling</a>
                 <div className="dropdown-divider"></div>
-                <a className="dropdown-item" href="/">Something else here</a>
+                <a className="dropdown-item" href="/">Other wines</a>
               </NavDropdown>
             </li>
           </ul>
           <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search wines</button>
           </form>
         </div>
       </nav>
