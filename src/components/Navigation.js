948463fb6bd0993e977feca3453bda32c7a0bb7d
jss530 <jsifre01@gmail.com>
2018-05-29 21:32:36 -0400

    added navdropdown component and got dropdown working

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,19 @@
-
 import React from 'react';
+import NavDropdown from './NavDropdown'
+
+const NavItem = props => {
+  const pageURI = window.location.pathname+window.location.search
+  const liClassName = (props.path === pageURI) ? "nav-item active" : "nav-item";
+  const aClassName = props.disabled ? "nav-link disabled" : "nav-link"
+  return (
+    <li className={liClassName}>
+      <a href={props.path} className={aClassName}>
+        {props.name}
+        {(props.path === pageURI) ? (<span className="sr-only">(current)</span>) : ''}
+      </a>
+    </li>
+  );
+}
 
 class Navigation extends React.Component {
   render() {
@@ -13,26 +27,18 @@ class Navigation extends React.Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">Link</a>
-            </li>
+
+            <NavItem path="/" name="Home" />
+            <NavItem path="/page2" name="Page2" />
+            <NavItem path="/page3" name="Disabled" disabled="true" />
+
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <NavDropdown name="Dropdown">
                 <a className="dropdown-item" href="/">Action</a>
                 <a className="dropdown-item" href="/">Another action</a>
                 <div className="dropdown-divider"></div>
                 <a className="dropdown-item" href="/">Something else here</a>
-              </div>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="/">Disabled</a>
+              </NavDropdown>
             </li>
           </ul>
           <form className="form-inline my-2 my-lg-0">
