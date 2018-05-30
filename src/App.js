d928392c8b33e471faac170d8980a3fcf1e0764d
jss530 <jsifre01@gmail.com>
2018-05-29 20:26:30 -0400

    finished initial navigation component, added to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Front Page</h1>
+      <div>
+        <Navigation />
+        <div className="container">
+          <h1>Front Page</h1>
+        </div>
       </div>
     );
   }
