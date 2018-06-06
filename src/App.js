353f63429a022be1c19c3322e85a375006b39be6
jss530 <jsifre01@gmail.com>
2018-06-06 19:10:27 -0400

    Got sidebar and main to display responsively

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Sidebar from './components/Sidebar';
 import Main from './components/Main';
+import {Container, Row, Col} from 'reactstrap';
 
 
 class App extends Component {
@@ -10,9 +11,12 @@ class App extends Component {
     return (
       <div>
         <Navigation />
-        <div className="container">
-           <Main />
-        </div>
+        <Container>
+          <Row>
+            <Col> <Sidebar /> </Col>
+            <Col sm="12" md="8"> <Main /> </Col> /*specify that I want Main to take up screen for smaller screens */
+          </Row>
+        </Container>
       </div>
     );
   }
