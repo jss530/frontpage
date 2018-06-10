9a5ef46ded6612bdfbc5121ea1e49da6b87a799f
jss530 <jsifre01@gmail.com>
2018-06-10 19:40:57 -0400

    Scrapped carousel for Image component testing. Add sidebar css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import './App.css';
 import Navigation from './components/Navigation';
 import Sidebar from './components/Sidebar';
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import { Grid, Row, Col, Carousel, Carousel.Item, Carousel.Caption } from 'react-bootstrap';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
 import sparkling from '../details/img/sparkling.jpg';
 import redWine from '../details/img/Wine-1.jpg';
 import tours from '../details/img/Wine-tour.jpg';
+import './Sidebar.css';
 
 class Sidebar extends React.Component {
   render() {
     return (
-        <Grid>
-          <Row>
-            <Col>
-              <h3>Here's what's new:</h3>
-              <Carousel>
-                <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src="/carousel.png" />
-                  <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src="/carousel.png" />
-                  <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                  <img width={900} height={500} alt="900x500" src="/carousel.png" />
-                  <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                  </Carousel.Caption>
-                </Carousel.Item>
-              </Carousel>
-            </Col>
-          </Row>
-        </Grid>
-
+      <Grid>
+        <Row>
+          <Image src={ redWine } className="sidebar-image" />
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <Image src={ tours } className="sidebar-image" />
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <Image src={ sparkling } className="sidebar-image" />
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Row>
+      </Grid>
     );
   };
 };
