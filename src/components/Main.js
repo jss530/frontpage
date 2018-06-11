e162d9802c896ff5e0f2a6fadb699b4077b97737
jss530 <jsifre01@gmail.com>
2018-06-10 20:39:46 -0400

    Changed carousel photo. Moved to using bootstrap 3 in main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import { Media } from 'reactstrap';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
 import profile from '../details/img/main.jpg';
 import './Main.css';
 
@@ -8,11 +7,11 @@ import './Main.css';
 class Main extends React.Component {
   render() {
     return (
-      <div>
-        <Media>
-          <Media object src={ profile } alt="Main image" className="image"/>
-        </Media>
-      </div>
+      <Grid fluid={true}>
+        <Row>
+          <Image src={ profile } alt="Main image" className="image"/>
+        </Row>
+      </Grid>
     );
   };
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import sparkling from '../details/img/sparkling.jpg';
-import redWine from '../details/img/Wine-1.jpg';
+import redWine from '../details/img/reds.jpg';
 import tours from '../details/img/Wine-tour.jpg';
 import './Sidebar.css';
 
@@ -15,7 +15,7 @@ class Sidebar extends React.Component {
             <img width={900} height={500} alt="red-wines" src={ redWine } />
             <Carousel.Caption>
               <h3>New reds!</h3>
-              <p className="dark-font">Go to the red wine category to see what's new!</p>
+              <p>Go to the red wine category to see what's new!</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
